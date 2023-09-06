a208a5d5094fed2974ed13f73fad4b3f322cb47d
Phannghi <phanphucnghi9a3thcspt2002@gmail.com>
2023-09-06 22:32:21 +0700

    add login, home and productDetail

---
+++ Webwood-frontend/assets/js/index.js
@@ -0,0 +1,15 @@
+// const header1 = document.querySelector('.header');
+
+// let lastScrollTop = 0;
+
+// window.addEventListener('scroll', () => {
+//     const currentScrollTop = window.scrollY;
+
+//     if (currentScrollTop > lastScrollTop) {
+//         header1.classList.add('is--hidden');
+//     } else {
+//         header1.classList.remove('is--hidden');
+//     }
+
+//     lastScrollTop = currentScrollTop;
+// });
